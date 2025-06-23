cd8cf138b1d9cb3727b2f61ab06b568f94dcc9ac
Emilse Arias <emilsearias@gmail.com>
2025-06-23 11:28:30 -0300

    solicitiamos la frase de un personaje usando fetch y pasamos sus datos por props

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,48 @@
-import { Button, Container } from "react-bootstrap"
-import Frase from "./components/Frase"
-import logo from './assets/logosimpson.png'
+import { Button, Container } from "react-bootstrap";
+import Frase from "./components/Frase";
+import logo from "./assets/logosimpson.png";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [personaje, setPersonaje] = useState({});
+
+  useEffect(() => {
+    //todas las lineas que quiero que se ejecuten solo en montaje
+    obtenerFrase()
+  }, []);
+
+  const obtenerFrase = async () => {
+    try {
+      //lo que quiero que si suceda
+      console.log("aqui deberia pedir la frase");
+      const respuesta = await fetch(
+        "https://thesimpsonsquoteapi.glitch.me/quotes"
+      );
+      if (respuesta.status === 200) {
+        console.log(respuesta);
+        const datos = await respuesta.json();
+        console.log(datos[0]);
+        //la respuesta de la api la guardo dentro del state
+        setPersonaje(datos[0])
+      } else {
+        console.log("ocurrio un error en el llamado de la api");
+      }
+    } catch (error) {
+      console.error(error)
+      //aqui agregaria alguna logica para mostrar un mensaje al usuario final
+    }
+  };
+
   return (
     <Container className="text-center my-4">
-     <img src={logo} alt="Logo de los simpsons" className="w-100" />
-     <h1 className="display-3 my-4">Frase simpsons</h1>
-     <Frase></Frase>
-     <Button variant="warning" className="mt-4">Obtener Frase</Button>
+      <img src={logo} alt="Logo de los simpsons" className="w-100" />
+      <h1 className="display-3 my-4">Frase simpsons</h1>
+      <Frase personaje={personaje}></Frase>
+      <Button variant="warning" className="mt-4">
+        Obtener Frase
+      </Button>
     </Container>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Frase.jsx
+++ src/components/Frase.jsx
@@ -1,13 +1,13 @@
 import Card from "react-bootstrap/Card";
 
-const Frase = () => {
+const Frase = ({personaje}) => {
   return (
     <Card className="align-items-center">
-      <Card.Img variant="top" src="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FNelsonMuntz.png?1497567511185" className="w-50 personaje-img"/>
+      <Card.Img variant="top" src={personaje.image} className="w-50 personaje-img"/>
       <Card.Body>
-        <Card.Title>Nelson Muntz</Card.Title>
+        <Card.Title>{personaje.character}</Card.Title>
         <Card.Text>
-          Shoplifting is a victimless crime, like punching someone in the dark.
+          {personaje.quote}
         </Card.Text>
       </Card.Body>
     </Card>
