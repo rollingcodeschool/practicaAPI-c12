1344490befa46372f3efb851d9eb74d10da544b5
Emilse Arias <emilsearias@gmail.com>
2025-06-25 09:43:56 -0300

    agregamos un spinner como componente condicional

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
-import { Button, Container } from "react-bootstrap";
+import { Button, Container, Spinner } from "react-bootstrap";
 import Frase from "./components/Frase";
 import logo from "./assets/logosimpson.png";
 import { useState, useEffect } from "react";
 
 function App() {
   const [personaje, setPersonaje] = useState({});
+  const [mostrarSpinner, setMostrarSpinner] = useState(true);
 
   useEffect(() => {
     //todas las lineas que quiero que se ejecuten solo en montaje
-    obtenerFrase()
+    obtenerFrase();
   }, []);
 
   const obtenerFrase = async () => {
     try {
+      setMostrarSpinner(true)
       //lo que quiero que si suceda
       console.log("aqui deberia pedir la frase");
       const respuesta = await fetch(
@@ -23,12 +25,13 @@ function App() {
         const datos = await respuesta.json();
         console.log(datos[0]);
         //la respuesta de la api la guardo dentro del state
-        setPersonaje(datos[0])
+        setPersonaje(datos[0]);
+        setMostrarSpinner(false)
       } else {
         console.log("ocurrio un error en el llamado de la api");
       }
     } catch (error) {
-      console.error(error)
+      console.error(error);
       //aqui agregaria alguna logica para mostrar un mensaje al usuario final
     }
   };
@@ -37,7 +40,13 @@ function App() {
     <Container className="text-center my-4">
       <img src={logo} alt="Logo de los simpsons" className="w-100" />
       <h1 className="display-3 my-4">Frase simpsons</h1>
-      <Frase personaje={personaje}></Frase>
+      {mostrarSpinner ? (
+        <div className="my-5">
+          <Spinner animation="grow" variant="light" />
+        </div>
+      ) : (
+        <Frase personaje={personaje}></Frase>
+      )}
       <Button variant="warning" className="mt-4" onClick={obtenerFrase}>
         Obtener Frase
       </Button>
